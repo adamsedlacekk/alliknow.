4c2d7b4d9ed1055c486525bcf8f3b129fd943d58
adamsedlacekk <adamek.sedlacek12@gmail.com>
2026-07-28 20:11:57 +0200

    Delete 1 directory

--- 1/1.2/BalistickaOchrana.cpp
+++
@@ -1,9 +0,0 @@
-#include "Vybaveni.h"
-#include "BalistickaOchrana.h"
-#include <iostream>
-#include <string>
-
-BalistickaOchrana :: BalistickaOchrana(std::string kod, double kg, int odolnost) : Vybaveni(kod, kg), tridaOdolnosti(odolnost){}
-void BalistickaOchrana::pripravKAkci(){
-    std::cout << "Kontrola celistvosti balisticke ochrany " << kodOznaceni << " (trida odolnosti: T" << tridaOdolnosti << ")." << std::endl;
-}
